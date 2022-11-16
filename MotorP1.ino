5843cd0a75959b45d559e09a1c0d3fb848995b38
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2022-11-16 13:18:28 -0600

    mejorado

--- MotorP1.ino
+++ MotorP1.ino
@@ -4,13 +4,17 @@ int Y;
 int PULSADOR = 9;	
 int SW;			
 int motorIzq=11;
+int motorDer=10;
 
 char opcion;
 Servo motoIzq;
+Servo motoDer;
 void setup(){
     pinMode(motorIzq, OUTPUT);
+    pinMode(motorDer, OUTPUT);
     pinMode(PULSADOR, INPUT);	
     motoIzq.attach(motorIzq);
+    motoDer.attach(motorDer);
     Serial.begin(9600);
 }
 void loop(){
@@ -19,33 +23,37 @@ void loop(){
   SW = digitalRead(PULSADOR);
   if(X > 430 && X < 570){
     if (Y > 520 && Y <= 1023){	
-    Serial.println("Mapeo y arriba: ");			// si Y esta en la zona de arriba
-    controlMotorIzq(map(Y, 0, 480, -255 , 0));	
+      Serial.println("Mapeo y arriba: ");			// si Y esta en la zona de arriba
+      controlMotorIzq(map(Y, 0, 480, -255 , 0));	
+      controlMotorDer(map(Y, 0, 480, -255 , 0));
     }  
     if (Y >= 0 && Y < 480){					// si Y esta en la zona de abajo
       Serial.println("Mapeo y abajo: ");
       controlMotorIzq(map(Y, 520, 1023, 0 , 255));
+      controlMotorDer(map(Y, 520, 1023, 0 , 255));
     } 
   }
-  if (X >= 0 && X < 430){					// si X esta en la zona izquierda
-    Serial.println("Mapeo x izquierda: ");
-  
-    controlMotorIzq(map(X, 520, 1023, 0 , 255));
+
+  if(Y > 430 && Y < 570){
+    if (X >= 0 && X < 430){					// si X esta en la zona izquierda
+      Serial.println("Mapeo x izquierda: ");
+      controlMotorIzq(map(X, 520, 1023, 0 , 255));
+      controlMotorDer(map(X, 520, 1023, 0 , -255));
 
 
-  } 
-  if (X > 570 && X <= 1023){					// si X esta en la zona derecha
-    Serial.println("Mapeo x derecha: ");
- 
-    controlMotorIzq(map(X, 0, 480, -255 , 0));	
-  } 
-
-  
+    } 
+    if (X > 570 && X <= 1023){					// si X esta en la zona derecha
+      Serial.println("Mapeo x derecha: ");
+      controlMotorIzq(map(X, 0, 480, -255 , 0));	
+      controlMotorDer(map(X, 0, 480, 255 , 0));
+    } 
+  }
   
   
   if ((Y >  500 && Y <= 520) && (X >  500 && X <= 520)){	
     Serial.println("Mapeo muerta  ");			// si Y esta en la zmuerta
     controlMotorIzq(0);	
+    controlMotorDer(0);
   } 
   //PLANO       X      Y
   //ADELANTE = 510    1023
@@ -58,6 +66,9 @@ void loop(){
 }
 
 int controlMotorIzq(int value){
-  motoIzq.write(map(value,-255,255,1000,2000));
+  motoIzq.write(map(value,-255,255,2000,1000));
+}
+int controlMotorDer(int value){
+  motoDer.write(map(value,-255,255,1000,2000));
 }
 
